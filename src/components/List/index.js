243f3a93443b19c86851bca064389ccb8f763eee
Alysson Reis <al.reis@live.com>
2020-08-11 13:41:59 +0100

    Backup

---
+++ src/components/List/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #711;
+      text-decoration: none;
+    }
+  }
+`;
+
+export default List;
